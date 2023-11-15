466511cc0602a9cbafe5d50460b54d7e17da3e6d
wvitorsj <wvitorsj08@gmail.com>
2023-11-15 20:04:24 -0300

    Projeto Criado

---
+++ src/main/java/com/wvitorsj/dslistbackendaula/DslistBackendAulaApplication.java
@@ -0,0 +1,13 @@
+package com.wvitorsj.dslistbackendaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackendAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistBackendAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvitorsj/dslistbackendaula/DslistBackendAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvitorsj.dslistbackendaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
