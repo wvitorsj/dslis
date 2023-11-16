088b9087bcf7d799166c6aa51acac35635bf4b6d
wvitorsj <wvitorsj08@gmail.com>
2023-11-16 02:12:36 -0300

    GET game by id

--- src/main/java/com/wvitorsj/dslistbackendaula/services/GameService.java
+++ src/main/java/com/wvitorsj/dslistbackendaula/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.wvitorsj.dslistbackendaula.dto.GameDTO;
 import com.wvitorsj.dslistbackendaula.dto.GameMinDTO;
 import com.wvitorsj.dslistbackendaula.entities.Game;
 import com.wvitorsj.dslistbackendaula.repositories.GameRepository;
@@ -15,11 +17,18 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
        List<Game> result = gameRepository.findAll();
        
        return result.stream().map(x -> new GameMinDTO(x)).toList();
-
     }
     
 }
